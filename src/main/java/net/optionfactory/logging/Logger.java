682fc04d1e107c5dbe492fa74c671c691656f06b
Simone Baruzza <simone.baruzza@optionfactory.net>
2018-03-02 14:31:05 +0100

    :new: use slf4j wrapper; :new: lazy evaluation of parameters

--- src/main/java/net/optionfactory/logging/Logger.java
+++ src/main/java/net/optionfactory/logging/Logger.java
@@ -1,73 +1,119 @@
 package net.optionfactory.logging;
-
-import org.apache.log4j.Priority;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.spi.LocationAwareLogger;
 
 public class Logger {
 
-    private final org.apache.log4j.Logger inner;
+    private final LocationAwareLogger inner;
+    private static final String LOGGER_FQCN = Logger.class.getName();
+    private static final Marker NO_MARKER = null;
+    private static final Throwable NO_THROWABLE = null;
+    private static final Object[] NO_PARAMS = null;
 
-    public static Logger getLogger(Class clazz) {
-        return new Logger(clazz);
+    private Logger(Class clazz) {
+        this.inner = (LocationAwareLogger) LoggerFactory.getLogger(clazz);
     }
 
-    public Logger(org.apache.log4j.Logger inner) {
-        this.inner = inner;
+    public static Logger getLogger() {
+        try {
+            throw new RuntimeException();
+        } catch (RuntimeException expected) {
+            try {
+                final StackTraceElement[] stackTrace = expected.getStackTrace();
+                final Class clazz = Class.forName(stackTrace[1].getClassName());
+                return new Logger(clazz);
+            } catch (ClassNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
     }
 
-    public Logger(Class clazz) {
-        this(org.apache.log4j.Logger.getLogger(clazz));
+    public void trace(String template, Object... params) {
+        if (inner.isTraceEnabled()) {
+            if (params.length > 0 && (params[params.length - 1] instanceof Throwable)) {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.TRACE_INT, String.format(template, params), NO_PARAMS, (Throwable) params[params.length - 1]);
+            } else {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.TRACE_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+            }
+        }
     }
 
-    public void info(String message, Object... arguments) {
-        inner.info(String.format(message, arguments));
+    public void trace(String template, Supplier... suppliers) {
+        if (inner.isTraceEnabled()) {
+            final Object[] params = Stream.of(suppliers).map(s -> s.get()).toArray();
+            inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.TRACE_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+        }
     }
 
-    public void info(Throwable t, String message, Object... arguments) {
-        inner.info(String.format(message, arguments), t);
+    public void debug(String template, Object... params) {
+        if (inner.isDebugEnabled()) {
+            if (params.length > 0 && (params[params.length - 1] instanceof Throwable)) {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.DEBUG_INT, String.format(template, params), NO_PARAMS, (Throwable) params[params.length - 1]);
+            } else {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.DEBUG_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+            }
+        }
     }
 
-    public void debug(String message, Object... arguments) {
-        inner.debug(String.format(message, arguments));
+    public void debug(String template, Supplier... suppliers) {
+        if (inner.isDebugEnabled()) {
+            final Object[] params = Stream.of(suppliers).map(s -> s.get()).toArray();
+            inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.DEBUG_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+        }
     }
 
-    public void debug(Throwable t, String message, Object... arguments) {
-        inner.debug(String.format(message, arguments), t);
+    public void info(String template, Object... params) {
+        if (inner.isInfoEnabled()) {
+            if (params.length > 0 && (params[params.length - 1] instanceof Throwable)) {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.INFO_INT, String.format(template, params), NO_PARAMS, (Throwable) params[params.length - 1]);
+            } else {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.INFO_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+            }
+        }
     }
 
-    public void error(String message, Object... arguments) {
-        inner.error(String.format(message, arguments));
+    public void info(String template, Supplier... suppliers) {
+        if (inner.isInfoEnabled()) {
+            final Object[] params = Stream.of(suppliers).map(s -> s.get()).toArray();
+            inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.INFO_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+        }
     }
 
-    public void error(Throwable t, String message, Object... arguments) {
-        inner.error(String.format(message, arguments), t);
+    public void warn(String template, Object... params) {
+        if (inner.isWarnEnabled()) {
+            if (params.length > 0 && (params[params.length - 1] instanceof Throwable)) {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.WARN_INT, String.format(template, params), NO_PARAMS, (Throwable) params[params.length - 1]);
+            } else {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.WARN_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+            }
+        }
     }
 
-    public void trace(String message, Object... arguments) {
-        inner.trace(String.format(message, arguments));
+    public void warn(String template, Supplier... suppliers) {
+        if (inner.isWarnEnabled()) {
+            final Object[] params = Stream.of(suppliers).map(s -> s.get()).toArray();
+            inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.WARN_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+        }
     }
 
-    public void trace(Throwable t, String message, Object... arguments) {
-        inner.trace(String.format(message, arguments), t);
+    public void error(String template, Object... params) {
+        if (inner.isErrorEnabled()) {
+            if (params.length > 0 && (params[params.length - 1] instanceof Throwable)) {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.ERROR_INT, String.format(template, params), NO_PARAMS, (Throwable) params[params.length - 1]);
+            } else {
+                inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.ERROR_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+            }
+        }
     }
 
-    public void fatal(String message, Object... arguments) {
-        inner.fatal(String.format(message, arguments));
-    }
-
-    public void fatal(Throwable t, String message, Object... arguments) {
-        inner.fatal(String.format(message, arguments), t);
-    }
-
-    public void log(Priority priority, String message, Object... arguments) {
-        inner.log(priority, String.format(message, arguments));
-    }
-
-    public void log(Throwable t, Priority priority, String message, Object... arguments) {
-        inner.log(priority, String.format(message, arguments), t);
-    }
-
-    public org.apache.log4j.Logger getInner() {
-        return inner;
+    public void error(String template, Supplier... suppliers) {
+        if (inner.isErrorEnabled()) {
+            final Object[] params = Stream.of(suppliers).map(s -> s.get()).toArray();
+            inner.log(NO_MARKER, LOGGER_FQCN, LocationAwareLogger.ERROR_INT, String.format(template, params), NO_PARAMS, NO_THROWABLE);
+        }
     }
 
 }
